292cb91b6938cbbf89e9646e2d3a1735979e4ef3
Suvarna2005 <121661235+Suvarna2005@users.noreply.github.com>
2023-05-23 10:39:18 +0530

    import java.util.Scanner; public class Sample {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int x,y,z;         x=sc.nextInt();         y=sc.nextInt();         z=x*y;         System.out.format("%d",z);         sc.close();     } }

---
+++ Multiplication.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Sample
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int x,y,z;
+        x=sc.nextInt();
+        y=sc.nextInt();
+        z=x*y;
+        System.out.format("%d",z);
+        sc.close();
+    }
+}
