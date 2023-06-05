0d74aa57798ba5c695bcbc111406b516ea207948
Suvarna2005 <121661235+Suvarna2005@users.noreply.github.com>
2023-06-05 08:44:11 +0530

    import java.util.Scanner; public class Sample {     public static void main(String args[])     {         double a,b,c,s,r;         Scanner sc=new Scanner(System.in);         String str=sc.nextLine();         System.out.println(str);     } }

---
+++ Take_Input_And_Print.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Sample
+{
+    public static void main(String args[])
+    {
+        double a,b,c,s,r;
+        Scanner sc=new Scanner(System.in);
+        String str=sc.nextLine();
+        System.out.println(str);
+    }
+}
