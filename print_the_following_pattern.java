4b8232001ea2009c5a2fa976d26f0b88b9aede2a
Suvarna2005 <121661235+Suvarna2005@users.noreply.github.com>
2023-06-05 17:38:55 +0530

    import java.util.*; public class sample {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n;         n=sc.nextInt();         for(int i=1;i<=n;i++)         {             for(int j=1;j<=i;j++)             {                 System.out.print(j);             }             System.out.println();         }     } }

--- print_the_following_pattern.java
+++ print_the_following_pattern.java
@@ -8,9 +8,9 @@ public class sample
         n=sc.nextInt();
         for(int i=1;i<=n;i++)
         {
-            for(int j=n;j>0;j--)
+            for(int j=1;j<=i;j++)
             {
-                System.out.print(j+" ");
+                System.out.print(j);
             }
             System.out.println();
         }
